84f019b530b989f51df36670052e148d3aaf73a3
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-07-21 09:58:10 +0700

    Update and rename README.md to This is me!!!.lua

---
+++ This is me!!!.lua
@@ -0,0 +1,14 @@
+-[[
+  _________________
+ /                 \
+ |    ⬛     ⬛    |  Hi,I'm rechedmcvn#9168
+ |  ///       \\\  |
+ |       ____      |
+ \__   __   __   __/
+    |  | |  | |  |
+    |  | |  | |  |
+    \__/ \__/ \__/
+ ]]
+--[[
+I'm a script creator so thank you for me and my friends
+]]
